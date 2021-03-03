29cd901542357493a4b8dac75335c0ebf46d0508
Onur Karal <santobixua@gmail.com>
2021-03-04 00:57:22 +0300

    questiondetail started

--- src/pages/PollQuestion/index.js
+++ src/pages/PollQuestion/index.js
@@ -1,5 +1,8 @@
 import React,{useState,useEffect} from 'react';
 import {useParams} from 'react-router-dom';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import { useQuery } from '@apollo/client';
+import {QUESTION_DETAIL_SUBSCRIPTION} from './queries'
 
 function PollQuestion() {
     const {id} = useParams();
